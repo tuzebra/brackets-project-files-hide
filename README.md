cdf69c76823c9bf84d1f3aae2ccddcae9b490ac6
Huy <tuzebra@gmail.com>
2014-11-14 11:18:02 +0700

    Huy first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hide Project files panel
+A very simple extension for [Brackets](https://github.com/adobe/brackets/) to allow you to hide Project file panel.
+
---
+++ main.js
@@ -0,0 +1,7 @@
+/*jslint vars: true, plusplus: true, devel: true, regexp: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define, brackets, $ */
+
+define(function (require, exports, module) {
+    var ExtensionUtils = brackets.getModule('utils/ExtensionUtils')
+    ExtensionUtils.loadStyleSheet(module, 'styles/main.css');
+});
